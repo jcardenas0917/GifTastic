236aee9f006ab595699ee8e42b7e9146bcc575bb
jaime cardenas <jcardenas@fixcityrepairs.com>
2019-07-09 16:29:45 -0400

    started js code

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+
+var topic = ["scrubs","fresh prince","friends","the 70's show"];
+var userInput = $("#userInput").val();
+$("#gif1").text(userInput)
+console.log(userInput)
+
+function addTopic(){
+
+}
